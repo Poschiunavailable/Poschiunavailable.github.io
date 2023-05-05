ea80b71db96aee094d4734c888e21b0348617235
Poschiunavailable <patricktemborius@gmail.com>
2023-05-05 14:36:54 +0200

    working on portfolio item panel scaling

--- scripts.js
+++ scripts.js
@@ -84,46 +84,155 @@ function handleTypingTargets() {
     typingEffects.forEach((effect) => effect());
 }
 
-function lerp(start, end, amt) {
-    return (1 - amt) * start + amt * end
-}
+const lerp = (start, end, delta) => start * (1 - delta) + end * delta;
+
+
 
 const rotationStrength = 50;
 
+function lerpColor(source, target, alpha) {
+    let result = { r: 0, g: 0, b: 0, a: 0 };
+    result.r = lerp(source.r, target.r, alpha);
+    result.g = lerp(source.g, target.g, alpha);
+    result.b = lerp(source.b, target.b, alpha);
+    result.a = lerp(source.a, target.a, alpha);
+    return result;
+}
+
+
 function handlePortfolioItems() {
-    // Get all the elements with the class "portfolio-item"
     const portfolioItems = document.querySelectorAll(".portfolio-item");
+    const rotationStrength = 30;
+    const clickIndicator = document.createElement("div");
 
-    // Loop through each item and add a "mousemove" event listener
     portfolioItems.forEach((item) => {
-        const portfolioItems = document.querySelectorAll(".portfolio-item");
+        item.style.position = "relative";
+        item.appendChild(clickIndicator.cloneNode(true));
 
-        portfolioItems.forEach((item) => {
-            item.addEventListener("mousemove", (e) => {
-                const rect = item.getBoundingClientRect();
+        let isHovered = false;
+        let targetGradient = {
+            color1: { r: 49, g: 255, b: 52, a: 0.4 },
+            color2: { r: 100, g: 111, b: 154, a: 0.233 },
+        };
 
-                const relX = e.clientX - (rect.left + rect.width / 2);
-                const relY = (rect.top + rect.height / 2) - e.clientY;
+        let currentGradient = {
+            color1: { r: 49, g: 255, b: 52, a: 0.4 },
+            color2: { r: 100, g: 111, b: 154, a: 0.233 },
+        };
+        const updateGradient = () => {
+            currentGradient.color1 = lerpColor(currentGradient.color1, targetGradient.color1, 0.05);
+            currentGradient.color1 = lerpColor(currentGradient.color2, targetGradient.color2, 0.05);
 
-                let rotateY = 0;
-                let rotateX = 0;
+            item.style.backgroundImage = `linear-gradient(rgba(${currentGradient.color1.r}, ${currentGradient.color1.g}, ${currentGradient.color1.b}, ${currentGradient.color1.a}), rgba(${currentGradient.color2.r}, ${currentGradient.color2.g}, ${currentGradient.color2.b}, ${currentGradient.color2.a}))`;
 
-                rotateX = relY / rect.height * rotationStrength;
-                rotateY = relX / rect.width * rotationStrength;
+            requestAnimationFrame(updateGradient);
+        };
 
-                item.style.transform = `perspective(1000px) scale(1.5) rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+        const handleMove = (e, isTouch = false) => {
+            const rect = item.getBoundingClientRect();
 
-                let rotateXDelta = (rotateX + rotationStrength / 2) / rotationStrength;
+            const clientX = isTouch ? e.touches[0].clientX : e.clientX;
+            const clientY = isTouch ? e.touches[0].clientY : e.clientY;
 
-                console.log("rot x: " + rotateXDelta);
-                let bgImage = `linear-gradient(rgba(49, 255, 52,${lerp(0.4, 1, rotateXDelta).toFixed(3)}), rgb(72, 80, 112,${lerp(0.4, 1, rotateXDelta).toFixed(3)}));`;
-                console.log(bgImage);
-                item.style.backgroundImage = bgImage;
-            });
+            const relX = clientX - (rect.left + rect.width / 2);
+            const relY = (rect.top + rect.height / 2) - clientY;
 
-            item.addEventListener("mouseleave", () => {
-                item.style.transform = "";
-            });
+            let rotateY = 0;
+            let rotateX = 0;
+
+            rotateX = relY / rect.height * rotationStrength;
+            rotateY = relX / rect.width * rotationStrength;
+
+            // Calculate the available space
+            const availableSpaceLeft = rect.left;
+            const availableSpaceRight = window.innerWidth - (rect.left + rect.width);
+
+            // Calculate the scale based on the available space
+            const scale = Math.min(1.5, window.innerWidth / rect.width);
+
+            // Calculate the displacement
+            const maxDisplacement = (rect.width * scale - rect.width) / 2;
+            let translateX = 0;
+            if (availableSpaceLeft < maxDisplacement) {
+                translateX = maxDisplacement - availableSpaceLeft;
+            } else if (availableSpaceRight < maxDisplacement) {
+                translateX = availableSpaceRight - maxDisplacement;
+            }
+
+            item.style.transform = `perspective(1000px) scale(${scale}) translateX(${translateX}px) rotateY(${rotateY}deg) rotateX(${rotateX}deg)`;
+
+            let rotateXDelta = (rotateX + rotationStrength / 2) / rotationStrength;
+
+            //background shine effect when tilting upwards
+            let bgImage = ``;
+            if (isHovered) {
+                if (rotateXDelta > 0.75) {
+                    rotateXDelta = (rotateXDelta - 0.75) * 4;
+                    targetGradient.color1 = {
+                        r: lerp(49, 200, rotateXDelta),
+                        g: lerp(255, 255, rotateXDelta),
+                        b: lerp(52, 200, rotateXDelta),
+                        a: 1,
+                    };
+                    targetGradient.color2 = {
+                        r: lerp(100, 210, rotateXDelta),
+                        g: lerp(111, 200, rotateXDelta),
+                        b: lerp(154, 255, rotateXDelta),
+                        a: 1,
+                    };
+                } else {
+                    targetGradient.color1 = { r: 49, g: 255, b: 52, a: 1 };
+                    targetGradient.color2 = { r: 100, g: 111, b: 154, a: 1 };
+                }
+            }
+        };
+
+        item.addEventListener("mousemove", handleMove);
+
+        requestAnimationFrame(updateGradient);
+
+        item.addEventListener("mouseenter", () => {
+            isHovered = true;
+            targetGradient.color1.a = 1;
+            targetGradient.color2.a = 1;
+        });
+
+        item.addEventListener("mouseleave", () => {
+            isHovered = false;
+            targetGradient.color1.a = 0.4;
+            targetGradient.color2.a = 0.233;
+        });
+
+        item.addEventListener("mouseleave", () => {
+            item.style.transform = "";
+            item.style.backgroundImage = "linear-gradient(rgba(49, 255, 52, 0.4), rgba(72, 80, 112, 0.4))";
+            item.querySelector("div").style.opacity = "0";
+        });
+
+        item.addEventListener("mousedown", () => {
+            item.querySelector("div").style.transform = "translateX(-50%) scale(0.9)";
+        });
+
+        item.addEventListener("mouseup", () => {
+            item.querySelector("div").style.transform = "translateX(-50%) scale(1)";
+        });
+
+        item.addEventListener("touchstart", () => {
+            item.querySelector("div").style.transform = "translateX(-50%) scale(0.9)";
+        });
+
+        item.addEventListener("touchend", () => {
+            item.querySelector("div").style.transform = "translateX(-50%) scale(1)";
+
+        });
+        item.addEventListener("click", () => {
+            const portfolioDetail = document.getElementById("portfolio-detail");
+            portfolioDetail.style.display = "block";
+        });
+
+        const portfolioDetail = document.getElementById("portfolio-detail");
+        portfolioDetail.addEventListener("click", () => {
+            portfolioDetail.style.display = "none";
         });
     });
 }
