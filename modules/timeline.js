ed57a9634aa9ea16bdb91b1d45adaba316ec2965
Poschiunavailable <patricktemborius@gmail.com>
2024-03-19 04:17:01 +0100

    working on timeline functionality, fixing min/max year/month, fixing timeline slider, deactivated year for now

--- modules/timeline.js
+++ modules/timeline.js
@@ -1,11 +1,11 @@
 const timeData = {
-    currentDate: null,
     currentMonth: null,
     currentYear: null,
     minYear: null,
     maxYear: null,
     minMonth: null,
     maxMonth: null,
+    totalMonths: null,
 }
 
 const timelineElements = {
@@ -18,27 +18,61 @@ const timelineElements = {
 }
 
 function generateCVTimeline() {
+    timeData.minYear = Infinity;
+    timeData.maxYear = -Infinity;
 
-    timeData.currentDate = new Date();
-    timeData.currentMonth = timeData.currentDate.getMonth();
-    timeData.currentYear = timeData.currentDate.getFullYear();
-
-    timeData.minYear = timeData.currentYear;
-    timeData.maxYear = timeData.currentYear;
-
-    timeData.minMonth = timeData.currentMonth;
-    timeData.maxMonth = timeData.currentMonth;
+    timeData.minMonth = 12;
+    timeData.maxMonth = 1;
 
     timelineElements.projects.forEach(project => {
-        let [startYear] = project.dataset.start.split('-').map(Number);
-        let [endYear] = project.dataset.end.split('-').map(Number);
+        //parse project data into array with year [0] and month [1]
+        let startDate = project.dataset.start.split('-').map(Number);
+        let endDate = project.dataset.end.split('-').map(Number);
 
-        timeData.minYear = Math.min(timeData.minYear, startYear, endYear);
-        timeData.maxYear = Math.max(timeData.maxYear, startYear, endYear);
+        //define min and max year
+        timeData.minYear = Math.min(timeData.minYear, startDate[0], endDate[0]);
+        timeData.maxYear = Math.max(timeData.maxYear, startDate[0], endDate[0]);
 
-        timeData.minMonth = Math.min()
+        //define min month and max month
+        if (startDate[0] === timeData.minYear) {
+            timeData.minMonth = Math.min(timeData.minMonth, startDate[1]);
+        }
+        if (endDate[0] === timeData.minYear) {
+            timeData.minMonth = Math.min(timeData.minMonth, endDate[1]);
+        }
+        if (startDate[0] === timeData.maxYear) {
+            timeData.maxMonth = Math.max(timeData.maxMonth, startDate[1]);
+        }
+        if (endDate[0] === timeData.maxYear) {
+            timeData.maxMonth = Math.max(timeData.maxMonth, endDate[1]);
+        }
     });
 
+    timeData.currentMonth = timeData.minMonth;
+    timeData.currentYear = timeData.minYear;
+
+    timeData.totalMonths = ((timeData.maxYear - timeData.minYear) - 1) * 12 + timeData.maxMonth - 1 + (12 - timeData.minMonth);
+
+    console.log(timeData.totalMonths);
+
+    for (let month = timeData.totalMonths; month >= 0; month--) {
+        let monthDiv = document.createElement('div');
+        monthDiv.className = 'month';
+
+        let yearInsert = timeData.minYear + 1 + Math.floor((month - (timeData.maxMonth - 1)) / 12);
+        let monthInsert = (month + 2) % 12;
+
+        monthDiv.textContent = new Date(yearInsert, monthInsert).toLocaleString('default', { month: 'short' });
+        timeline.appendChild(monthDiv);
+
+        for (let subdivision = 4; subdivision >= 0; subdivision--) {
+            let subdivisionDiv = document.createElement('div');
+            subdivisionDiv.className = "subdivision";
+            monthDiv.appendChild(subdivisionDiv);
+        }
+    }
+
+    /*
     for (let year = timeData.maxYear; year >= timeData.minYear; year--) {
         let yearDiv = document.createElement('div');
         yearDiv.className = 'year';
@@ -61,6 +95,7 @@ function generateCVTimeline() {
             }
         }
     }
+    */
 
     let cvSectionRect = timelineElements.cvSection.getBoundingClientRect();
 
@@ -73,8 +108,6 @@ function generateCVTimeline() {
     });
 }
 
-generateCVTimeline();
-
 function updateTimelineElements(value) {
     timelineElements.currentTime.style.top = value;
     timelineElements.timelineMarkerLeft.style.top = value;
@@ -82,19 +115,14 @@ function updateTimelineElements(value) {
 }
 
 function updateTimelineScroll() {
-    let scrollY = window.scrollY;
-    let scrollYMax = scrollY + window.innerHeight;
-    let scrollCenter = scrollY + scrollYMax / 2;
     const halfScreenSize = window.innerHeight / 2;
-    // Calculate the position of the currentTime element within the limits of the timeline
     const timelineRect = timelineElements.timeline.getBoundingClientRect();
     const timelineTop = timelineRect.top;
     const timelineBottom = timelineRect.bottom;
 
     // Calculate the monthsScrolled
-    let timeScrolledPercentage = (timelineBottom - halfScreenSize) / (Math.abs((timelineBottom - halfScreenSize)) + Math.abs((timelineTop - halfScreenSize)));
-    const totalMonths = timeData.maxYear * 12 + timeData.maxMonth + timeData.minMonth;
-    console.log(totalMonths);
+    let timeScrolledPercentage = 1 - ((timelineBottom - halfScreenSize) / (Math.abs((timelineBottom - halfScreenSize)) + Math.abs((timelineTop - halfScreenSize))));
+
     updateTimelineElements(`50%`);
 
     //handle window center is outside bounds of the timeline
@@ -104,12 +132,14 @@ function updateTimelineScroll() {
     }
     else if (window.innerHeight / 2 >= timelineBottom) {
         updateTimelineElements(`${timelineBottom}px`);
-        timeScrolledPercentage = 1;
+        timeScrolledPercentage = 0.99;
     }
-    let monthsScrolled = Math.floor(totalMonths * timeScrolledPercentage);
-    console.log(timeScrolledPercentage + "   " + (timelineTop - halfScreenSize) + "   " + (timelineBottom - halfScreenSize));
 
-    let newDate = new Date(timeData.currentYear, timeData.currentMonth - monthsScrolled);
+    let monthsScrolled = Math.floor((timeData.totalMonths + 1) * timeScrolledPercentage);
+
+    //console.log(timeScrolledPercentage + "   " + (timelineTop - halfScreenSize) + "   " + (timelineBottom - halfScreenSize) + "   " + monthsScrolled);
+
+    let newDate = new Date(timeData.maxYear, timeData.maxMonth - monthsScrolled);
     let newMonth = newDate.toLocaleString('default', { month: 'long' });
     let newYear = newDate.getFullYear();
     timelineElements.currentTime.textContent = `${newMonth} ${newYear}`;
